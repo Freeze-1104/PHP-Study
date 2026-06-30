10d79d994f1ae5ec1a0a35b1a45b72ab79f0430c
302A-03\cre <a@a>
2026-06-30 12:17:24 +0900

    こうしん　

--- works/workEx1/Gun.php
+++ works/workEx1/Gun.php
@@ -1,5 +1,6 @@
 <?php
-class Gun {
+class Gun
+{
 
     // ↓フィールド============================
     // 銃の名前
@@ -8,39 +9,87 @@ class Gun {
     private $maxMagazine;
     // 残弾
     private $currentMagazine;
+    private $extendedMagazine = 0;
     // ↑フィールド============================
 
     // コンストラクタ
-    function __construct($name, $maxMagazine) {
+    function __construct($name, $maxMagazine)
+    {
         // 問題1
+        $this->name = $name;
+        $this->maxMagazine = $maxMagazine;
+        $this->currentMagazine = 0;
     }
 
     // 現在の状態を表示
-    function echoStatus() {
-        echo "======現在の状態======". "\n";
+    function echoStatus()
+    {
+        echo "======現在の状態======" . "\n";
         echo "武器名: " . $this->name . "\n";
         echo "最大装弾数: " . $this->maxMagazine . "\n";
         echo "残弾数: " . $this->currentMagazine . "\n";
-        echo "======================". "\n";
+        echo "======================" . "\n";
     }
 
     // リロード
-    function reload() {
+    function reload()
+    {
         // 問題2
+        if ($this->currentMagazine == $this->maxMagazine) {
+            echo "リロードの必要はありません\n";
+        } else {
+            $this->currentMagazine = $this->maxMagazine;
+        }
     }
 
     // 発砲
-    function fire() {
+    function fire()
+    {
         // 問題3
+        if ($this->currentMagazine == 0) {
+            echo "リロードしてください\n";
+            return;
+        }
+
+        $this->currentMagazine--;
+
+        echo $this->name . "を発砲しました。残弾: " . $this->currentMagazine . "発\n";
+
+        if ($this->currentMagazine == 0) {
+            echo "リロードしてください\n";
+        }
     }
 
     // 拡張マガジンを装着
-    function setExtendedMagazine() {
+    function setExtendedMagazine($num)
+    {
         // 問題4
+        if (!is_int($num) || $num <= 0) {
+            echo "引数が不正です\n";
+            return;
+        }
+
+        $this->maxMagazine -= $this->extendedMagazine;
+
+        $this->extendedMagazine = $num;
+        $this->maxMagazine += $num;
     }
 
+
+
     // 拡張マガジンを取外し
-    function unsetExtendedMagazine() {
+    function unsetExtendedMagazine()
+    {
         // 問題4
+        if ($this->extendedMagazine == 0) {
+            echo "拡張マガジンは装着されていません\n";
+            return;
+        }
+        $this->maxMagazine -= $this->extendedMagazine;
+        $this->extendedMagazine = 0;
+
+        if ($this->currentMagazine > $this->maxMagazine) {
+            $this->currentMagazine = $this->maxMagazine;
+        }
     }
 }
--- works/workEx1/Main.php
+++ works/workEx1/Main.php
@@ -3,9 +3,40 @@
 // Userクラスの読み込み
 require 'Gun.php';
 
-$mainWeapon = new Gun("AK47",30);
-$subWeapon = new Gun("Mosin-Nagant",5);
+$mainWeapon = new Gun("AK47", 30);
+$subWeapon = new Gun("Mosin-Nagant", 5);
+
 
 // 現在の状態を表示
+$mainWeapon->echoStatus();
+$subWeapon->echoStatus();
+
+$mainWeapon->fire();
+$subWeapon->fire();
+
+$mainWeapon->reload();
+$subWeapon->reload();
+
+$mainWeapon->echoStatus();
+$subWeapon->echoStatus();
+
+$mainWeapon->fire();
+$subWeapon->fire();
+
+$mainWeapon->echoStatus();
+$subWeapon->echoStatus();
+
+$mainWeapon->setExtendedMagazine(100);
+$subWeapon->setExtendedMagazine(100);
+
+$mainWeapon->reload();
+$subWeapon->reload();
+
+$mainWeapon->echoStatus();
+$subWeapon->echoStatus();
+
+$mainWeapon->unsetExtendedMagazine();
+$subWeapon->unsetExtendedMagazine();
+
 $mainWeapon->echoStatus();
 $subWeapon->echoStatus();
